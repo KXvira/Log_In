ea414a5a2937a63599c2feee562af8b8e9d20c8e
KXvira <kevinkimurgor@kabarak.ac.ke>
2025-02-13 11:29:37 +0300

    feat: add LoginForm component and update App to use it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import LoginForm from "./Components/LoginForm/LoginForm";
 
 function App() {
   return (
     <div>
-      <p>Hello World</p>
+      <LoginForm />
     </div>
   );
 }
